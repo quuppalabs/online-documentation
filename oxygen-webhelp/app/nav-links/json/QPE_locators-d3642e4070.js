71a176f91b0e453c3e4f3e370df90e26f980d5be
Quuppa Documentation <support@quuppa.com>
2023-09-05 16:44:28 +0300

    installer updates

---
+++ oxygen-webhelp/app/nav-links/json/QPE_locators-d3642e4070.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Locator Details Table Definitions","href":"QPE\/topics\/QPE_locators_definitions.html","attributes":{"data-id":"QPE_locators_definitions"},"menu":{"hasChildren":false},"tocID":"QPE_locators_definitions-d3642e4093","topics":[]}]});
