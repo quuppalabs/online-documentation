f6e24e839fa3c2d421f21dd3a75d90921ed9257b
Quuppa Documentation <support@quuppa.com>
2023-09-05 15:14:38 +0300

    installer section updated

---
+++ oxygen-webhelp/app/nav-links/json/creating_a_Q17_focusing_locator-d3612e5992.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Reset to Factory Settings","href":"Q17\/topics\/q17_ig_reset_to_factory_settings.html","attributes":{"data-id":"q17_ig_reset_to_factory_settings"},"menu":{"hasChildren":false},"tocID":"q17_ig_reset_to_factory_settings-d3612e6015","topics":[]}]});
