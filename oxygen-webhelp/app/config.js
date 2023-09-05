1a092d230a0f238ffc9872475ddfb20d0be68b41
Quuppa Documentation <support@quuppa.com>
2023-09-05 16:50:15 +0300

    installer updates

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20230905164258';
+                args = '20230905164906';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
