f89cd6594aff4578d2670b122b60e33bd131b173
Quuppa Documentation <support@quuppa.com>
2023-09-05 15:14:38 +0300

    installer section updated

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20230822145704';
+                args = '20230905151053';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
---
+++ oxygen-webhelp/app/nav-links/json/QPE_troubleshooting-d3612e4535.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Record Binary Data for Quuppa Support","href":"QPE\/topics\/QPE_binary_logs.html","attributes":{"data-id":"QPE_binary_logs"},"menu":{"hasChildren":false},"tocID":"QPE_binary_logs-d3612e4558","topics":[]}]});
