90d9d23f5341feb36d776a6d3904bcfd4935e21e
Quuppa Documentation <support@quuppa.com>
2023-09-19 10:32:22 +0300

    8.5 doc changes

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20230905174016';
+                args = '20230919103054';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
