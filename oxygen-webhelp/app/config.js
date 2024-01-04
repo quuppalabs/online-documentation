784cc0922df84a373e3cbfb094601d0c663f9b33
Quuppa Documentation <support@quuppa.com>
2024-01-04 18:39:31 +0200

    Qt3 certification extra

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240104161508';
+                args = '20240104183148';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
