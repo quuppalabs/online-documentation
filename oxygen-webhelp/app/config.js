cdf80b8b682783785de566925ff162e7d3aa9982
Quuppa Documentation <support@quuppa.com>
2024-01-10 15:58:11 +0200

    dev-kit

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240110154456';
+                args = '20240110155528';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
