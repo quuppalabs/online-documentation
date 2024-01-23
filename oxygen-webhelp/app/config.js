4f6e5072239e227774942f926f1409f100f8a9f5
Quuppa Documentation <support@quuppa.com>
2024-01-23 13:15:33 +0200

    fixes again 2

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240123125619';
+                args = '20240123131217';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
---
+++ oxygen-webhelp/app/nav-links/json/QSP_udploggingapi_introduction-d3686e2850.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Location Data Filtering","href":"QSP\/topics\/QSP_udp_logging_api_filtering.html","attributes":{"data-id":"QSP_udp_logging_api_filtering"},"menu":{"hasChildren":false},"tocID":"QSP_udp_logging_api_filtering-d3686e2873","topics":[]}]});
