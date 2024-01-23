22a11ac61df19e1ce1a0c29b82f1e69131155c6f
Quuppa Documentation <support@quuppa.com>
2024-01-23 13:30:39 +0200

    test fix for v3 firmware

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240123131217';
+                args = '20240123132710';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
