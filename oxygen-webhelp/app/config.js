3c64869a7fdc1f8c469963c237886e171d5efa00
Quuppa Documentation <support@quuppa.com>
2024-01-29 10:06:53 +0200

    v3 provisioning correction

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240126165649';
+                args = '20240129100304';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
