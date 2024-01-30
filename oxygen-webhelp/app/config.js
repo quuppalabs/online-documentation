1c365952c0e9a2a118d333a6401d17f4ac854840
Quuppa Documentation <support@quuppa.com>
2024-01-30 15:02:11 +0200

    q-port reference

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240130142447';
+                args = '20240130143406';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
