1af315849d11273884fcfba9ad1e3980f3360ba0
Quuppa Documentation <support@quuppa.com>
2024-02-07 12:00:31 +0200

    updated Q-link ratios

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240207113144';
+                args = '20240207115847';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
