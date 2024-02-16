9bb280b223753ec63d36257e5222a252f1bf52ec
Quuppa Documentation <support@quuppa.com>
2024-02-16 17:18:57 +0200

    QT3 offline image update

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240208111933';
+                args = '20240216171637';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
