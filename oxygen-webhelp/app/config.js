03ce59ed2bd90123c0430cb559c7c10d38c23130
Quuppa Documentation <support@quuppa.com>
2024-03-25 10:40:41 +0200

    tag deisng principles update

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240320145504';
+                args = '20240325103725';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
