6b35781e3e76f74d4c9cc5f688cc015f17833748
Quuppa Documentation <support@quuppa.com>
2024-05-29 10:48:25 +0300

    9.1 updates

---
+++ oxygen-webhelp/app/nav-links/json/q17_ig_using_the_quuppa_q17-d3771e8390.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Q17定位器指示灯","shortdesc":"<p class=\"shortdesc\">下表显示了定位器指示灯指示的不同模式。<\/p>","href":"Q17_zh\/topics\/Q17_indicator_light.html","attributes":{"data-id":"q17_ig_led_status"},"menu":{"hasChildren":false},"tocID":"q17_ig_led_status-d3771e8413","topics":[]}]});
---
+++ oxygen-webhelp/app/nav-links/json/q17_ig_using_the_quuppa_q17-d3771e9895.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Q17 Locator 표시등","shortdesc":"<p class=\"shortdesc\">아래 표에는 Locator의 표시등을 통해 알 수 있는 여러 가지 Locator 모드가 나와 있습니다.<\/p>","href":"Q17_ko\/topics\/Q17_indicator_light.html","attributes":{"data-id":"q17_ig_led_status"},"menu":{"hasChildren":false},"tocID":"q17_ig_led_status-d3771e9918","topics":[]}]});
