575070a1bd79e2c78ba9eab42f368ef9735a1368
Quuppa Documentation <support@quuppa.com>
2025-06-19 13:21:58 +0300

    publish changes from Ilari and Niko

---
+++ oxygen-webhelp/app/nav-links/json/QPE_generation_Q_offline_module-d376e665.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Update a Project Using the Offline Module for Generation Q Locators","href":"QPE\/topics\/offline_module_update.html","attributes":{"data-id":"offline_module_update"},"menu":{"hasChildren":false},"tocID":"offline_module_update-d376e688","topics":[]}]});
