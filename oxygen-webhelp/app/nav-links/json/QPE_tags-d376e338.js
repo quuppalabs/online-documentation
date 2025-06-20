74c18442ca0bdd85ee21b6b80b3e428139b967b4
Kalle Korhonen <kalle.korhonen@quuppa.com>
2025-06-20 07:49:18 +0300

    Revert "publish changes from Ilari and Niko" - managed to publish only one section of the documentation
    This reverts commit 575070a1bd79e2c78ba9eab42f368ef9735a1368.

--- oxygen-webhelp/app/nav-links/json/QPE_tags-d376e338.js
+++
@@ -1,1 +0,0 @@
-define({"topics":[{"title":"Tags Details Table Definitions","href":"QPE\/topics\/QPE_tags_definitions.html","attributes":{"data-id":"QPE_tags_definitions"},"menu":{"hasChildren":false},"tocID":"QPE_tags_definitions-d376e361","topics":[]}]});
