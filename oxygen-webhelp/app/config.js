80a1efe6206769ac23b26d93f72de6bca15e5c66
Quuppa Documentation <support@quuppa.com>
2025-06-25 11:43:34 +0300

    publish full documentation with changes from Ilari and Niko

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240529104213';
+                args = '20250625114113';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
