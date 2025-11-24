e202ff7a8bfd39a836909b334cb2dd0af9fe4824
Quuppa Documentation <support@quuppa.com>
2025-11-24 14:30:17 +0200

    quuppa.qam -> *.qam

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20251124140511';
+                args = '20251124142420';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
